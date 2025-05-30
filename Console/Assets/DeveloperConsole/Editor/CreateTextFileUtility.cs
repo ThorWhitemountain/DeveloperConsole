03ce8a4a543013ca78398006c895374eac8813a0
Thor <72937268+ThorWhitemountain@users.noreply.github.com>
2025-05-30 10:40:49 +0200

    codestyle

--- Console/Assets/DeveloperConsole/Editor/CreateTextFileUtility.cs
+++ Console/Assets/DeveloperConsole/Editor/CreateTextFileUtility.cs
@@ -1,20 +1,23 @@
 ﻿#if UNITY_EDITOR
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-namespace Anarkila.DeveloperConsole {
-
-    public static class CreateTextFileUtility {
-
-        public static bool GenerateCommandList() {
-            if (Application.isPlaying) {
+namespace Anarkila.DeveloperConsole
+{
+    public static class CreateTextFileUtility
+    {
+        public static bool GenerateCommandList()
+        {
+            if (Application.isPlaying)
+            {
                 Debug.Log("Exit play mode to generate command list.");
                 return false;
             }
 
-            var path = GetScriptRootPath("CreateTextFileUtility", true);
+            string path = GetScriptRootPath("CreateTextFileUtility", true);
             path += "ConsoleCommands.txt";
             DeleteFile(path);
             MakeTextFile(path);
@@ -22,64 +25,77 @@ namespace Anarkila.DeveloperConsole {
             return true;
         }
 
-        private static void DeleteFile(string path) {
-            if (File.Exists(path)) {
+        private static void DeleteFile(string path)
+        {
+            if (File.Exists(path))
+            {
                 File.Delete(path);
-                UnityEditor.AssetDatabase.Refresh();
+                AssetDatabase.Refresh();
             }
         }
 
-        private static void MakeTextFile(string path) {
-
+        private static void MakeTextFile(string path)
+        {
             // Get all console commands
-            var commands = CommandDatabase.GetConsoleCommandAttributes(Debug.isDebugBuild, false);
+            List<ConsoleCommandData> commands = CommandDatabase.GetConsoleCommandAttributes(Debug.isDebugBuild, false);
 
             // make new txt file and write to it
-            StreamWriter writer = new StreamWriter(path, true);
+            StreamWriter writer = new(path, true);
             string separator = "------ \n";
             writer.WriteLine("All commands found with [ConsoleCommand] attribute in the current project.");
             writer.WriteLine("Commands found: " + commands.Count);
 
-            for (int i = 0; i < commands.Count; i++) {
+            for (int i = 0; i < commands.Count; i++)
+            {
                 writer.WriteLine(separator);
 
                 writer.WriteLine("Command name: " + commands[i].commandName);
 
-                if (commands[i].parameters == null || commands[i].parameters.Length == 0) {
+                if (commands[i].parameters == null || commands[i].parameters.Length == 0)
+                {
                     writer.WriteLine("Parameter: None");
                 }
-                else {
-                    for (int k = 0; k < commands[i].parameters.Length; k++) {
-                        var x = string.Format("Parameter {0}: {1}", k + 1, commands[i].parameters[k].FullName);
+                else
+                {
+                    for (int k = 0; k < commands[i].parameters.Length; k++)
+                    {
+                        string x = string.Format("Parameter {0}: {1}", k + 1, commands[i].parameters[k].FullName);
                         writer.WriteLine(x);
                     }
                 }
+
                 string defaultValue = commands[i].defaultValue;
-                if (!string.IsNullOrEmpty(defaultValue)) {
+                if (!string.IsNullOrEmpty(defaultValue))
+                {
                     writer.WriteLine("Default value: " + defaultValue);
                 }
 
                 writer.WriteLine("Script name: " + commands[i].scriptNameString + ".cs");
 
                 string methodType;
-                if (commands[i].isStaticMethod) {
+                if (commands[i].isStaticMethod)
+                {
                     methodType = "static void ";
                 }
-                else if (commands[i].isCoroutine) {
+                else if (commands[i].isCoroutine)
+                {
                     methodType = "IEnumerator ";
                 }
-                else {
+                else
+                {
                     methodType = "void ";
                 }
 
                 writer.WriteLine("Method name: " + methodType + commands[i].methodName);
 
 
-                if (commands[i].hiddenCommand) {
+                if (commands[i].hiddenCommand)
+                {
                     writer.WriteLine("is hidden command: " + commands[i].hiddenCommand);
                 }
 
-                if (commands[i].hiddenCommandMinimalGUI) {
+                if (commands[i].hiddenCommandMinimalGUI)
+                {
                     writer.WriteLine("is hidden minimal GUI: " + commands[i].hiddenCommandMinimalGUI);
                 }
             }
@@ -89,25 +105,28 @@ namespace Anarkila.DeveloperConsole {
             // refresh Unity assets and select/highlight new txt file
             AssetDatabase.ImportAsset(path);
             EditorUtility.FocusProjectWindow();
-            UnityEngine.Object obj = AssetDatabase.LoadAssetAtPath(path, typeof(UnityEngine.Object));
+            Object obj = AssetDatabase.LoadAssetAtPath(path, typeof(Object));
             Selection.activeObject = obj;
 
             Debug.Log("Command list generated to path: " + path);
         }
 
-        private static string GetScriptRootPath(string scriptname, bool parse = false) {
-
-            if (string.IsNullOrEmpty(scriptname)) {
+        private static string GetScriptRootPath(string scriptname, bool parse = false)
+        {
+            if (string.IsNullOrEmpty(scriptname))
+            {
                 return "";
             }
 
-            var filter = string.Format("t:Script {0}", scriptname);
-            var asset = AssetDatabase.FindAssets(filter);
-            var path = AssetDatabase.GUIDToAssetPath(asset[0]);
+            string filter = string.Format("t:Script {0}", scriptname);
+            string[] asset = AssetDatabase.FindAssets(filter);
+            string path = AssetDatabase.GUIDToAssetPath(asset[0]);
 
-            if (parse) {
+            if (parse)
+            {
                 int index = path.LastIndexOf("/");
-                if (index >= 0) {
+                if (index >= 0)
+                {
                     path = path.Substring(0, index + 1);
                 }
             }
--- Console/Assets/DeveloperConsole/Editor/DeveloperConsoleEditor.cs
+++ Console/Assets/DeveloperConsole/Editor/DeveloperConsoleEditor.cs
@@ -3,68 +3,82 @@ using System.Linq;
 using UnityEngine;
 using UnityEditor;
 
-namespace Anarkila.DeveloperConsole {
-
+namespace Anarkila.DeveloperConsole
+{
     [CustomEditor(typeof(DeveloperConsole))]
-    public class DeveloperConsoleEditor : Editor {
-
+    public class DeveloperConsoleEditor : Editor
+    {
         private const string VERSION = "Developer Console 1.0.2";
-        private string[] tabs = new string[] { "All Settings", "GUI Settings", "General Settings", "Keybindings", "Debug Settings" };
+
+        private string[] tabs = new string[]
+            { "All Settings", "GUI Settings", "General Settings", "Keybindings", "Debug Settings" };
+
         private SerializedObject sTarget;
         private int toolbarTab;
 
-        private List<SerializedProperty> guiSettings = new List<SerializedProperty>();
-        private List<SerializedProperty> generalSettings = new List<SerializedProperty>();
-        private List<SerializedProperty> keybindings = new List<SerializedProperty>();
-        private List<SerializedProperty> debugSettings = new List<SerializedProperty>();
+        private List<SerializedProperty> guiSettings = new();
+        private List<SerializedProperty> generalSettings = new();
+        private List<SerializedProperty> keybindings = new();
+        private List<SerializedProperty> debugSettings = new();
 
         private DeveloperConsole console;
         private bool renderCustomGUI = true;
         private int theme = 0;
+        private string input;
 
-        private void OnEnable() {
+        private void OnEnable()
+        {
             console = (DeveloperConsole)target;
             sTarget = new SerializedObject(target);
 
             RegisterSerializedProperties();
         }
 
-        private void RegisterSerializedProperties() {
+        private void RegisterSerializedProperties()
+        {
             generalSettings.Clear();
             debugSettings.Clear();
             guiSettings.Clear();
             keybindings.Clear();
 
-            var fieldValues = console.settings.GetType().GetFields().Select(f => f.Name).ToList();
+            List<string> fieldValues = console.settings.GetType().GetFields().Select(f => f.Name).ToList();
             string setting = "settings.";
-            for (int i = 0; i < fieldValues.Count; i++) {
-                var name = setting + fieldValues[i];
-                if (0 <= i && i <= 8) {
+            for (int i = 0; i < fieldValues.Count; i++)
+            {
+                string name = setting + fieldValues[i];
+                if (0 <= i && i <= 8)
+                {
                     AppendToList(guiSettings, name);
                 }
-                else if (9 <= i && i <= 34) {
+                else if (9 <= i && i <= 34)
+                {
                     AppendToList(generalSettings, name);
                 }
-                else if (35 <= i && i <= 38) {
+                else if (35 <= i && i <= 38)
+                {
                     AppendToList(keybindings, name);
                 }
-                else {
+                else
+                {
                     AppendToList(debugSettings, name);
                 }
             }
         }
 
-        private void AppendToList(List<SerializedProperty> list, string name) {
+        private void AppendToList(List<SerializedProperty> list, string name)
+        {
             list.Add(sTarget.FindProperty(name));
         }
 
-        public override void OnInspectorGUI() {
+        public override void OnInspectorGUI()
+        {
             EditorGUILayout.LabelField(VERSION, EditorStyles.boldLabel);
 
             renderCustomGUI = EditorGUILayout.Toggle("Custom GUI", renderCustomGUI);
             GUILayout.Space(20);
 
-            if (!renderCustomGUI) {
+            if (!renderCustomGUI)
+            {
                 DrawDefaultInspector();
                 return;
             }
@@ -77,15 +91,18 @@ namespace Anarkila.DeveloperConsole {
 
             RenderTab();
 
-            if (EditorGUI.EndChangeCheck()) {
+            if (EditorGUI.EndChangeCheck())
+            {
                 sTarget.ApplyModifiedProperties();
             }
 
             RenderBottomButtons();
         }
 
-        private void RenderTab() {
-            switch (toolbarTab) {
+        private void RenderTab()
+        {
+            switch (toolbarTab)
+            {
                 case 0: RenderAll(); break;
                 case 1: RenderGUISettings(); break;
                 case 2: RenderGeneralSettings(); break;
@@ -94,32 +111,44 @@ namespace Anarkila.DeveloperConsole {
             }
         }
 
-        private void RenderBottomButtons() {
+        private void RenderBottomButtons()
+        {
             GUILayout.Space(20);
-            if (GUILayout.Button("Reset Settings", GUILayout.Height(30))) {
+            if (GUILayout.Button("Reset Settings", GUILayout.Height(30)))
+            {
                 console.settings = new ConsoleSettings();
                 EditorUtility.SetDirty(console.gameObject);
                 Debug.Log("Settings reset to default.");
             }
 
             GUILayout.Space(10);
-            if (GUILayout.Button("Generate Command List", GUILayout.Height(30))) {
+            if (GUILayout.Button("Generate Command List", GUILayout.Height(30)))
+            {
                 CreateTextFileUtility.GenerateCommandList();
             }
 
             // Show 'Next GUI theme button' when playing in Editor
             if (Application.isPlaying) {
 
+            // Show 'Next GUI theme button' when playing in Editor
+            if (Application.isPlaying)
+            {
                 GUILayout.Space(10);
-                if (GUILayout.Button("Next GUI Theme", GUILayout.Height(30))) {
+                if (GUILayout.Button("Next GUI Theme", GUILayout.Height(30)))
+                {
                     ++theme;
-                    if (theme == 3) theme = 0;
+                    if (theme == 3)
+                    {
+                        theme = 0;
+                    }
+
                     Console.SetGUITheme((ConsoleGUITheme)theme);
                 }
 
                 GUILayout.Space(10);
-                if (GUILayout.Button("Write messages to file", GUILayout.Height(30))) {
-                    var msgs = Console.GetConsoleMessagesArray();
+                if (GUILayout.Button("Write messages to file", GUILayout.Height(30)))
+                {
+                    string[] msgs = Console.GetConsoleMessagesArray();
                     TextFileWriter.WriteToFile(msgs);
                 }
             }
@@ -132,31 +161,44 @@ namespace Anarkila.DeveloperConsole {
             RenderDebugSettings();
         }
 
-        private void RenderGUISettings() {
+        private void RenderGUISettings()
+        {
             bool customTheme = console.settings.interfaceTheme == ConsoleGUITheme.Custom;
-            for (int i = 0; i < guiSettings.Count; i++) {
-                if (!customTheme && i == 8) {
+            for (int i = 0; i < guiSettings.Count; i++)
+            {
+                if (!customTheme && i == 8)
+                {
                     continue;
                 }
+
                 EditorGUILayout.PropertyField(guiSettings[i]);
             }
         }
 
-        private void RenderGeneralSettings() {
+        private void RenderGeneralSettings()
+        {
             Render(generalSettings);
         }
 
-        private void RenderKeybindingSettings() {
+        private void RenderKeybindingSettings()
+        {
             Render(keybindings);
         }
 
-        private void RenderDebugSettings() {
+        private void RenderDebugSettings()
+        {
             Render(debugSettings);
         }
 
-        private void Render(List<SerializedProperty> list) {
-            for (int i = 0; i < list.Count; i++) {
-                if (list[i] == null) continue;
+        private void Render(List<SerializedProperty> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == null)
+                {
+                    continue;
+                }
+
                 EditorGUILayout.PropertyField(list[i]);
             }
         }
